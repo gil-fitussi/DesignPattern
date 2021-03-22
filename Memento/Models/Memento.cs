4bd7c38405d3506316615163c9cd849db86faa20
Gil <gigo1985@gmail.com>
2021-03-22 12:42:57 +0200

    Add Memento Pattern

---
+++ Memento/Models/Memento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento.Models
+{
+    public class Memento
+    {
+        public Student Student { get; set; }
+
+        public Memento(Student student)
+        {
+            Student = student.Clone();
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,29 @@
+﻿using Memento.Models;
+using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.ChangeStatus("Admited");
+            Console.WriteLine($"Initial Student Status: {student.Status}");
+            try
+            {
+                student.ChangeStatus("Start Proccessing..");
+                Console.WriteLine($"Student Status After Proccess: {student.Status}");
+                throw new Exception();
+            }
+            catch (Exception)
+            {
+                student.Revert();
+            }
+            Console.WriteLine($"Student Status After Error: {student.Status}");
+            Console.WriteLine($"Student Status After Error: {student.GetRevertStudent().Status}");
+
+            Console.ReadLine();
+        }
+    }
+}
