0c1f143e8bf112a305edad272a58cc74f76b3ae5
Gil <gigo1985@gmail.com>
2021-03-22 14:21:24 +0200

    Add template method

---
+++ Template_Method/Models/UniversityBardak.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template_Method.Models
+{
+    public class UniversityBardak : CourseRegistration
+    {
+        protected override void ProcessPayment()
+        {
+            Console.WriteLine("Process CC Payment..");
+        }
+
+        protected override void SendNotification()
+        {
+            Console.WriteLine("Send an letter..");
+        }
+
+        protected override void VerifyPrerequisites()
+        {
+            Console.WriteLine("Validate records..");
+        }
+    }
+}
---
+++ Template_Method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Template_Method.Models;
+
+namespace Template_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var universityBalagan = new UniversityBalagan();
+            universityBalagan.RegisterStudent();
+           
+            
+            var universityBardak = new UniversityBardak();
+            universityBardak.RegisterStudent();
+
+            Console.ReadLine();
+        }
+    }
+}
